4ba8f4f08a5b05419501386b75bdce02d06aa8f7
Muhammad Ahsan <mahsank@cryptolab.net>
2017-07-21 12:45:55 +0300

    Limit the text to 80 columns in all the problems in problem set 1

--- PS1/problem2.py
+++ PS1/problem2.py
@@ -1,19 +1,18 @@
 '''
 Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl', then your program should print
 
 Number of times bob occurs is: 2
 
 '''
 
-temp = 0
-count = 0
+temp = 0 count = 0
 
 #s = 'azcbobobegghakl'
 
-string_len = len('bob')
-s_len = len(s)
+string_len = len('bob') s_len = len(s)
 
 for i in range(temp,s_len):
     if temp <= s_len - string_len:
