e6c2245ae7dc48153f62999d9c7b2374ab69f872
Muhammad Ahsan <mahsank@cryptolab.net>
2017-08-05 00:38:43 +0300

    Solution of problem 3 in Final Exam

---
+++ FinalExam/problem3.py
@@ -0,0 +1,26 @@
+'''
+Implement a function that meets the specifications below.
+
+def sum_digits(s):
+    """ assumes s a string
+        Returns an int that is the sum of all of the digits in s.
+          If there are no digits in s it raises a ValueError exception. """
+    # Your code here
+For example, sum_digits("a;35d4") returns 12.
+
+'''
+
+def sum_digits(s):
+    """ assumes s a string
+        Returns an int that is the sum of all of the digits in s.
+          If there are no digits in s it raises a ValueError exception. """
+
+    str_list = list(s)
+    count = 0
+    for i in str_list:
+        if i.isdigit():
+            count += int(i)
+    if s != '0' and count == 0:
+        raise ValueError()
+    else:
+        return count
